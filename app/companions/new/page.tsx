79d48c858a8debb2aac246ce52661856113f17b5
al1211 <anil.kumar0372915@gmail.com>
2025-12-30 19:04:39 +0530

    add clerak authentication

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,19 @@
+"use client"
+
+
+import CompanionForm from '@/components/CompanionForm'
 import React from 'react'
+
+
 
 const NewCompanion = () => {
   return (
-    <div>
-     new companions 
-    </div>
+    <main className='w-1/3 md:w-2/3 items-center justify-center'>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Companion Builder</h1>
+        <CompanionForm/>
+      </article>
+    </main>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,50 @@
+import CompanionsCard from '@/components/CompanionsCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-
-    <h1 className='text-2xl underline'>Welcome to my app</h1>
-    <Button>
-      Let's get Started
-    </Button>
-    </div>
+   <main>
+      <h1 className='text-2xl underline' >Popular Companions</h1>
+      <section className='home-section'>
+       <CompanionsCard
+       id="123"
+       name="Neura the Brainy Explorer"
+       topic=" Neural Network of the Brain"
+       subject="Science"
+       duration={45}
+       color="#ffdafe"
+       />
+         <CompanionsCard
+       id="234"
+       name="Countsy the Number Wizard"
+       topic=" Derivatives & Integrals"
+       subject="Math"
+       duration={30}
+       color="#e5d0ff"
+       />
+         <CompanionsCard
+       id="456"
+       name="Verba the Vocabulary Builder"
+       topic="English Literature"
+       subject="Language"
+       duration={30}
+       color="#BDE7FF"
+       />
+       
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+         title="Recently completed session"
+         companions={recentSessions}
+         classNames="w-2/3 max-lg:w-full"
+        />
+         <CTA/>
+      </section>
+    </main>
   )
 }
 
